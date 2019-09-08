7951cc4b056f2555aecf79e24fc3452cd64da193
Mike Oleynik <oleynik.mike@yandex.ru>
2019-09-08 14:57:01 +0300

    chore: Made authentication for all controllers (transfer to ApplicationController)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :authenticate_user!
   # GET /
   def homepage
     render json: { success: 'ok' }, status: :ok
