209a94b4e62d64225ed311c4fa57d66c8ef4fe9b
Mike Oleynik <oleynik.mike@yandex.ru>
2019-09-08 16:47:15 +0300

    chore: Removing default value for user fullname

--- db/migrate/20190908064945_create_users.rb
+++ db/migrate/20190908064945_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :fullname, null: false, default: ''
+      t.string :fullname, null: false
       t.boolean :mentor, default: true
     end
   end
