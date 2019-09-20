34dcbb6d87803fd1b480d370ae248941e9d2403d
Mike Oleynik <oleynik.mike@yandex.ru>
2019-09-20 15:07:30 +0300

    chore: adding uniq validate for creating user of task

--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -3,4 +3,7 @@
 class UserTask < ApplicationRecord
   belongs_to :user
   belongs_to :task
+
+  validates :user_id, uniqueness: { scope: :task_id,
+                                    message: 'The student is already doing this task' }
 end
